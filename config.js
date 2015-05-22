000e46bb0886c4cc8b67a20ded6afa84337c46e3
Matt Mankins <mankins@gmail.com>
2015-05-22 13:53:16 +0200

    change default endpoint address.

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 'use strict';
 
-var path = require('path');
-
 function envOrDefault(key, def) {
 	return process.env[key] || def;
 }
 
 module.exports = {
-	endpoint: envOrDefault('ADIEU_ENDPOINT', 'https://api.adieu.io'),
+	endpoint: envOrDefault('ADIEU_ENDPOINT', 'https://www.adieu.io'),
 	// @type {Winston.Logger} or any object which defines methods for each of
 	// the npm loglevels.
 	logger: require('./lib/logger')
